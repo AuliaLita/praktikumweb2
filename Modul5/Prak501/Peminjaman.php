149fe63e3afc69be4ad0b01b6af41a1c4863eeb7
AuliaLita <101611847+AuliaLita@users.noreply.github.com>
2022-05-16 01:09:03 +0800

    Add files via upload

---
+++ Modul5/Prak501/Peminjaman.php
@@ -0,0 +1,46 @@
+<?php 
+require('./Model.php');
+if (isset($_GET['id_peminjaman'])){
+    hapuspeminjaman($_GET['id_peminjaman']);
+}
+?>
+
+<html>
+<head>
+    <title>Data Peminjaman</title>
+    <style>
+        table{
+            border-collapse: collapse;
+        }
+        tr{
+            border: black;
+        }
+        th{
+            background-color: #00FFFF;
+        }
+        td{
+            background-color: #F0FFFF;
+        }
+    </style>
+</head>
+<body>
+    <table border = "2">
+        <thead>
+            <tr>
+                <th>ID Peminjaman</th>
+                <th>Tanggal Peminjaman</th>
+                <th>Tanggal Kembali</th>
+                <th>Aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            tampilpeminjaman();
+            ?>
+        </tbody>
+    </table>
+    <br>
+    <a href="formpeminjaman.php"><button>Tambah Data Peminjaman</button></a> <br></br>
+    
+</body>
+</html>
